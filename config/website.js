762365c2f4841e38e409f78a128d748c8c018dc9
Hafizh Pratama <hafizhpratama99.com>
2020-04-15 15:47:44 +0700

    edit tampilan home

--- config/website.js
+++ config/website.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
+  siteTitle: 'Hafizh Pratama', // Navigation and Site Title
   siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
   siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteUrl: 'hafizhpratama.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription: 'This is where they post things!',
@@ -23,6 +23,6 @@ module.exports = {
   // Social component
   twitter: 'https://twitter.com/eggheadio/',
   twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  github: 'https://github.com/hafizhpratama/',
   linkedin: '',
 }
